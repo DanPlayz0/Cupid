26b49685e91b9047098b437cf5a6ee00c4ff8fcb
DanPlayz0 <damuffin36@gmail.com>
2022-12-11 09:15:28 +0000

    Added preview feature and a story `Love in Bloom`

--- commands/games/madlib.js
+++ commands/games/madlib.js
@@ -16,6 +16,21 @@ const stories = {
     much. First of all, there are all the [shape]
     decorations, and everything is covered in [color]`,
     lastUpdate: '12-2022-test'
+  },
+  story2: {
+    name: "Love in Bloom",
+    variables: [
+      { name: "Name", placeholder: "person named [name] decided to" },
+      { name: "Action", placeholder: "They [action] a bouquet of" },
+      { name: "Item", placeholder: "bouquet of [item], and waited" },
+      { name: "Emotion", placeholder: "was overcome with [emotion] and" },
+      { name: "Adjective", placeholder: "believe the [adjective] effort their" },
+    ],
+    text: stripIndents`Once upon a morning on Valentine's Day, a person named [name] decided to surprise their loved one with a gesture. 
+    They [action] a bouquet of [item], and waited anxiously for their partner to arrive home. 
+    When they finally did, the loved one was overcome with [emotion] and couldn't believe the [adjective] effort their partner had put in. 
+    It was a day they would never forget.`,
+    lastUpdate: '12-2022-test'
   }
 }
 
@@ -35,6 +50,16 @@ module.exports = class extends Command {
             name: `#${index+1} - ${customValue.name}`, value: customId
           }))
         },
+        {
+          name: "action",
+          description: "Preview or fill it out.",
+          type: 3,
+          required: false,
+          choices: [
+            { name: "Fill (default)", value: "fill" },
+            { name: "Preview", value: "preview" },
+          ]
+        },
       ],
       defer: false,
     });
@@ -43,6 +68,9 @@ module.exports = class extends Command {
   async run(ctx) {
     const storyArg = ctx.args.getString('story')
     const story = stories[storyArg];
+    if(ctx.args.getString('action') == "preview") 
+      return ctx.sendMsg(story.text.replace(/(\[[^\]]+\])/g, '__**$1**__'), {ephemeral: true});
+
     ctx.interaction.showModal({
       title: story.name,
       custom_id: `madlib_${storyArg}_${story.lastUpdate}`,
