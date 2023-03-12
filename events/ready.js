47931eef6aac0ddbb86bec166b265caa90109778
DanPlayz0 <damuffin36@gmail.com>
2023-03-12 08:37:29 +0000

    auto-detect change slash

--- events/ready.js
+++ events/ready.js
@@ -23,7 +23,26 @@ module.exports = class extends Event {
       client.site.listen(client.config.restapi.port);
     }
     
-    // if(client.guilds.cache.has('783178035322159156')) client.guilds.cache.get('783178035322159156').commands.set(client.commands.map(m=>m.commandData))
-    // client.application.commands.set(client.commands.map(m=>m.commandData));
+    const checkApplicationCommands = async (client) => {
+      const clientCommands = await client.application.commands.fetch();
+      client.savedCommands = clientCommands;
+      const commandsDiff = [...Object.values(clientCommands.toJSON()).filter((command) => {
+          const cmd = client.commands.get(command.name)?.commandData;
+          if(!cmd) return true;
+          if(command.description !== cmd.description) return true;
+          if(command.options.length !== cmd.options.length) return true;
+          const discordOptions = command.options.map(x=>`${x.type}|${x.name}|${x.description}|${x.required}|${x.choices?.length}|${x.options?.length}`);
+          const localOptions = cmd.options.map(x=>`${x.type}|${x.name}|${x.description}|${x.required??false}|${x.choices?.length}|${x.options?.length}`);
+          if(discordOptions.filter((x,i) => localOptions[i] !== x).length) return true;
+          return false;
+        }),
+        ...[...client.commands.keys()].filter(x=> !Object.values(clientCommands.toJSON()).some(a => a.name === x))
+      ]
+      if(commandsDiff.length) {
+        client.savedCommands = await client.application.commands.set(client.commands.map(x=>x.commandData));
+        console.log('Detected a change, posting slash commands.');
+      }
+    };
+    checkApplicationCommands(client);
   }
 }
